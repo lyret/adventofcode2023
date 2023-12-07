d6fb4cec8315a96740757ca3a6bbc6b6f36c51a0
Lyret <viktor@lyresten.se>
2023-12-07 13:45:01 +0100

    Editor config and run script

---
+++ test.mjs
@@ -0,0 +1,52 @@
+import * as Path from "node:path";
+import * as FS from "node:fs";
+
+// Test the given solution when executed
+(async () => {
+  const solution = process.argv[2];
+
+  // Determine the given solution to test
+  if (!solution) {
+    throw new Error(
+      "Please specify what solution to test, i.e. by running 'npm test 24'"
+    );
+  }
+
+  const folder = Path.resolve(".", solution);
+  const file = Path.resolve(".", solution, "index.mjs");
+
+  // Import solve function
+  if (!FS.existsSync(folder)) {
+    throw new Error(
+      `There is no solution folder (${solution}) for given the solution'`
+    );
+  }
+  if (!FS.existsSync(file)) {
+    throw new Error(
+      `There is no solution file (${solution}/index.mjs) for given the solution'`
+    );
+  }
+
+  const { solve } = await import(file);
+
+  if (!solve) {
+    throw new Error(
+      `There is no exported function named 'solve' in the solution file (${solution}/index.mjs)'`
+    );
+  }
+
+  // Find all inputs in the given solution folder
+  const inputs = FS.readdirSync(folder)
+    .filter((file) => file.indexOf(".txt") > -1)
+    .reverse()
+    .map((file) => ({
+      name: file.split(".")[0].toUpperCase(),
+      input: FS.readFileSync(Path.resolve(folder, file), "utf-8"),
+    }));
+
+  console.log("SOLUTIONS:");
+  for (const { name, input } of inputs) {
+    console.log(name);
+    console.log(await solve(input));
+  }
+})();
